c262b056f581401be5c8c94f8d6087ef64fe47f8
Mijin Jeon <mayihelpu27@gmail.com>
2018-08-11 15:33:03 +0900

    Feat: Add IBActions for mart selection buttons

--- MartHoliday/MartHoliday/MartSelectViewController.swift
+++ MartHoliday/MartHoliday/MartSelectViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MartSelectViewController: UIViewController {
 
+    @IBOutlet weak var emartButton: UIButton!
+    @IBOutlet weak var lotteMartButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class MartSelectViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func emartButtonTapped(_ sender: Any) {
+        guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "searchVC") as? SearchViewController else { return }
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
 
+
+    @IBAction func lotteMartButtonTapped(_ sender: Any) {
+    }
 }
 
