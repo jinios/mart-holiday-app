ddef3ebba095567ce94f5e0791633dc35594050b
Mijin Jeon <mayihelpu27@gmail.com>
2018-08-12 18:57:36 +0900

    Feat: Implement tableView #5

--- MartHoliday/MartHoliday/ViewController/SearchViewController.swift
+++ MartHoliday/MartHoliday/ViewController/SearchViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     var list: BranchList?
     var mart: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         setNavigationBarTitle()
     }
 
@@ -27,4 +30,25 @@ class SearchViewController: UIViewController {
         self.navigationItem.title = mart
     }
 
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let list = self.list else { return 0 }
+        return list.count()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let branchCell = tableView.dequeueReusableCell(withIdentifier: "branchCell", for: indexPath) as? BranchTableViewCell else { return UITableViewCell() }
+        guard let list = self.list else { return UITableViewCell() }
+        branchCell.branchData = list.branches[indexPath.row]
+        return branchCell
+    }
 }
