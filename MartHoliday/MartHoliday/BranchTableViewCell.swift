9abc3275de7ca7f0119ec8d4c3d86eb5a7e8a370
Mijin Jeon <mayihelpu27@gmail.com>
2018-08-12 18:53:46 +0900

    Feat: Implement BranchTableViewCell #5

---
+++ MartHoliday/MartHoliday/BranchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BranchTableViewCell.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 2018. 8. 12..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class BranchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
