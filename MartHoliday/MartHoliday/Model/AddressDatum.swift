d34d2bd16dc7a77d367c9495e3f1490e5e5dc4e6
Mijin Jeon <mayihelpu27@gmail.com>
2018-08-29 15:35:00 +0900

    Feat: Implement Codable objects inside AddressDtum  #21

--- MartHoliday/MartHoliday/Model/AddressDatum.swift
+++ MartHoliday/MartHoliday/Model/AddressDatum.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+struct AddressDatum: Codable {
+    var result: Result
+
+    func geoPoint() -> GeoPoint {
+        return result.items[0].point
+    }
+}
+
+struct Result: Codable {
+    var total: Int
+    var userquery: String
+    var items: [Item]
+}
+
+struct Item: Codable {
+    var address: String
+    var addrdetail: AddressDetail
+    var isRoadAddress: Bool
+    var point: GeoPoint
+}
+
+struct AddressDetail: Codable {
+    var country: String
+    var sido: String
+    var sigugun: String
+    var dongmyun: String
+    var ri: String
+    var rest: String
+}
+
+struct GeoPoint: Codable {
+    var x: Double
+    var y: Double
+}
