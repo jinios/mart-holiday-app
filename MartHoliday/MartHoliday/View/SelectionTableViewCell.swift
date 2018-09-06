7be9a88ffef9e0d5c1c148f9c4fdeac2ccfbf75e
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-06 22:53:54 +0900

    Add Assets and custom tableView cell #25

---
+++ MartHoliday/MartHoliday/View/SelectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectionTableViewCell.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 2018. 9. 6..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class SelectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
