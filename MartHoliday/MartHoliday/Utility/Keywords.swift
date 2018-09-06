aa8851dffa0910d1fe486f42b7cba00f7fe665ee
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-06 16:48:46 +0900

    Feat: Add URLHolder protocol and cases of enum #28
    - Add URLHolder protocol
    - Add new enum cases: homeplus, homeplusExpress

--- MartHoliday/MartHoliday/Utility/Keywords.swift
+++ MartHoliday/MartHoliday/Utility/Keywords.swift
@@ -8,15 +8,19 @@
 
 import Foundation
 
-enum Mart: String, KoreanName, JSONfile {
+enum Mart: String, KoreanName, JSONfile, URLHolder {
 
     case emart
     case lottemart
+    case homeplus
+    case homeplusExpress
 
     var koreanName: String {
         switch self {
         case .emart: return "이마트"
         case .lottemart: return "롯데마트"
+        case .homeplus: return "홈플러스"
+        case .homeplusExpress: return "홈플러스 익스프레스"
         }
     }
 
@@ -24,6 +28,16 @@ enum Mart: String, KoreanName, JSONfile {
         switch self {
         case .emart: return "emartList"
         case .lottemart: return "lottemartList"
+        default: return ""
+        }
+    }
+
+    var url: URL? {
+        switch self {
+        case .emart: return URL(string: "http://ec2-13-209-38-224.ap-northeast-2.compute.amazonaws.com/api/mart/EMART/list")
+        case .lottemart: return URL(string: "http://ec2-13-209-38-224.ap-northeast-2.compute.amazonaws.com/api/mart/LOTTEMART/list")
+        case .homeplus: return URL(string: "http://ec2-13-209-38-224.ap-northeast-2.compute.amazonaws.com/api/mart/HOMEPLUS/list")
+        case .homeplusExpress: return URL(string: "http://ec2-13-209-38-224.ap-northeast-2.compute.amazonaws.com/api/mart/HOMEPLUS_EXPRESS/list")
         }
     }
 }
@@ -34,4 +48,8 @@ protocol KoreanName {
 
 protocol JSONfile {
     var JSONfile: String { get }
+}
+
+protocol URLHolder {
+    var url: URL? { get }
 }
