7b41f90a1146fbbe8595a3c70e6a7585bde9972d
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-10 17:21:21 +0900

    Test: Add Test files

---
+++ MartHoliday/MartHolidayTests/MartHolidayTests.swift
@@ -0,0 +1,25 @@
+//
+//  MartHolidayTests.swift
+//  MartHolidayTests
+//
+//  Created by YOUTH2 on 2018. 9. 10..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import XCTest
+
+class MartHolidayTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
