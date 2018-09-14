2e8956f0c8ad8d387a4ce4c84de76afb9f445880
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-14 19:45:26 +0900

    Test: Add basic Unit Test

--- MartHoliday/MartHolidayTests/MartHolidayTests.swift
+++ MartHoliday/MartHolidayTests/MartHolidayTests.swift
@@ -10,16 +10,12 @@ import XCTest
 
 class MartHolidayTests: XCTestCase {
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDeletedFavorite() {
+        let favorite = FavoriteList.shared()
+        favorite.push(id: 1)
+        favorite.push(id: 40)
+        favorite.push(id: 323)
+        XCTAssertEqual([1,40,323], favorite.ids())
     }
     
 }
