341a7d9bce950028c3880e47b175bf404cb3ddab
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-21 01:14:40 +0900

    Feat: Add HolidayTableViewCell file #36

---
+++ MartHoliday/MartHoliday/View/HolidayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HolidayTableViewCell.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 2018. 9. 20..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class HolidayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
