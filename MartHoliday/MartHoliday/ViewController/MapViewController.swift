b8b6259d4a209d471dce94d1edfed790ebd4c78b
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-22 16:52:25 +0900

    Feat: Implement MartMapViewHolder, MapVC #37
    - Implement MartMapViewHolder protocol: for ViewControllers that holds
    MartMapView, MapViewDelegate

--- MartHoliday/MartHoliday/ViewController/MapViewController.swift
+++ MartHoliday/MartHoliday/ViewController/MapViewController.swift
@@ -8,28 +8,37 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+protocol MartMapViewHolder {
+    var mapView: NMapView? { get set }
+    var mapViewDelegate: MartMapDelegate? { get set }
+    func setDelegate(mapView: NMapView, mapViewDelegate: MartMapDelegate)
+}
+
+class MapViewController: UIViewController, MartMapViewHolder {
+
+    var mapViewDelegate: MartMapDelegate?
+    var mapView: NMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let mapView = mapView else { return }
+        guard let mapViewDelegate = mapViewDelegate else { return }
+        setDelegate(mapView: mapView, mapViewDelegate: mapViewDelegate)
+        self.view.addSubview(mapView)
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.prefersLargeTitles = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setDelegate(mapView: NMapView, mapViewDelegate: MartMapDelegate) {
+        mapView.setDelegate(mapViewDelegate)
     }
-    */
 
 }
