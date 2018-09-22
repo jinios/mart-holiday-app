3fe36c44501051d6172db7bb15e840043dbe83e0
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-22 14:58:44 +0900

    Feat: Add MartMapView swift file #37

---
+++ MartHoliday/MartHoliday/View/MartMapView.swift
@@ -0,0 +1,21 @@
+//
+//  MartMapView.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 2018. 9. 21..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class MartMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
