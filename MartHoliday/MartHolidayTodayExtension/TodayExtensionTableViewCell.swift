f9996f2e23c2de95efb64808d9a6ce6919d283be
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-23 16:55:59 +0900

    Feat: Modify selection style

--- MartHoliday/MartHolidayTodayExtension/TodayExtensionTableViewCell.swift
+++ MartHoliday/MartHolidayTodayExtension/TodayExtensionTableViewCell.swift
@@ -14,13 +14,7 @@ class TodayExtensionTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        self.selectionStyle = .none
     }
 
     func setData(branch: Branch) {
