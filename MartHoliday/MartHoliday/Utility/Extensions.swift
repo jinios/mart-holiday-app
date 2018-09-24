b899e9e3f12fb72553edfe27c6d4908e7d09017d
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-24 19:03:47 +0900

    Delete unused notification name

--- MartHoliday/MartHoliday/Utility/Extensions.swift
+++ MartHoliday/MartHoliday/Utility/Extensions.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 extension Notification.Name {
-    static let favoriteChanged = Notification.Name("favoriteChanged")
     static let slideMenuClose = Notification.Name("slideMenuClose")
     static let slideMenuTapped = Notification.Name("slideMenuClose")
 }
