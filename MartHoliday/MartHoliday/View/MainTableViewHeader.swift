bc301336523765e19fd8b38a3561fc9e2fbfc3fd
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-26 17:14:08 +0900

    Feat: Implement TableView Header #38

--- MartHoliday/MartHoliday/View/MainTableViewHeader.swift
+++ MartHoliday/MartHoliday/View/MainTableViewHeader.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2018년 JINiOS. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MainTableViewHeader: UITableViewHeaderFooterView {
+    @IBOutlet weak var nameLabel: UILabel!
+
+    var name: String! {
+        didSet {
+            self.nameLabel.text = name
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
