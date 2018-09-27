4a0a32b2099f3eac68a67ff4e85659667b1feae4
Mijin Jeon <mayihelpu27@gmail.com>
2018-09-28 00:23:13 +0900

    Add NoDataView file

---
+++ MartHoliday/MartHoliday/View/NoDataView.swift
@@ -0,0 +1,21 @@
+//
+//  NoDataView.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 2018. 9. 28..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class NoDataView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
