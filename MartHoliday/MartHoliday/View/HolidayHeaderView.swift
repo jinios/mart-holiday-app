a9cea2b95558588fdc2d8e1154e8549d6371dedf
Mijin Jeon <mayihelpu27@gmail.com>
2018-10-01 01:06:31 +0900

    Refactor: Add xib and DetailHeaderView #42

---
+++ MartHoliday/MartHoliday/View/HolidayHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HolidayHeaderView.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 2018. 10. 1..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class HolidayHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
