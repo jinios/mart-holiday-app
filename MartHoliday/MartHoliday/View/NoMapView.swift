d858d74c0d463d0e33c870234c9dc508091cf465
Mijin Jeon <mayihelpu27@gmail.com>
2018-10-03 00:44:21 +0900

    Feat: Add files to implement map error view #44

---
+++ MartHoliday/MartHoliday/View/NoMapView.swift
@@ -0,0 +1,21 @@
+//
+//  NoMapView.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 2018. 10. 2..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class NoMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
