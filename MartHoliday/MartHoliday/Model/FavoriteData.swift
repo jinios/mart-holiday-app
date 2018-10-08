159bdfcce90e5b5553de5abeb168e65bfc092941
Mijin Jeon <mayihelpu27@gmail.com>
2018-10-08 16:59:28 +0900

    Feat: Implement APIDataComparable,FavoriteData #47
    For comparing FavoriteData- token and favoriteList info from DB

--- MartHoliday/MartHoliday/Model/FavoriteData.swift
+++ MartHoliday/MartHoliday/Model/FavoriteData.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+protocol APIDataComparable: Equatable {
+    var token: String {get}
+    var favorites: [Int] {get}
+}
+
+class FavoriteData: APIDataComparable {
+
+    var token: String
+    var favorites: [Int]
+
+    init(token: String, favorites: [Int]) {
+        self.token = token
+        self.favorites = favorites
+    }
+
+    static func == (lhs: FavoriteData, rhs: FavoriteData) -> Bool {
+        return (lhs.token == rhs.token) && (lhs.favorites == rhs.favorites)
+    }
+
+    func isSame(otherData: FavoriteData) -> Bool {
+        return self == otherData
+    }
+
+}
