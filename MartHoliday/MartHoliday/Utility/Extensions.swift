afd1797b355ac4f121c4c99e91c5643c3e3fa163
Mijin Jeon <mayihelpu27@gmail.com>
2019-01-15 00:21:40 +0900

    Feat: randomElement() in Collection type #73

--- MartHoliday/MartHoliday/Utility/Extensions.swift
+++ MartHoliday/MartHoliday/Utility/Extensions.swift
@@ -118,3 +118,11 @@ extension NMapView {
 
 }
 
+extension Collection where Index == Int {
+
+    func randomElement() -> Iterator.Element? {
+        return isEmpty ? nil : self[Int(arc4random_uniform(UInt32(endIndex)))]
+    }
+
+}
+
