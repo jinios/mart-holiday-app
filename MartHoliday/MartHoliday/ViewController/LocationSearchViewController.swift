c17564c9cec6542d508c29ddca1eddeb353a42f1
Mijin Jeon <mayihelpu27@gmail.com>
2019-02-09 19:09:11 +0900

    Minor fix

--- MartHoliday/MartHoliday/ViewController/LocationSearchViewController.swift
+++ MartHoliday/MartHoliday/ViewController/LocationSearchViewController.swift
@@ -8,44 +8,13 @@
 
 import UIKit
 
-class LocationSearchViewController: UIViewController, NMapViewDelegate, NMapPOIdataOverlayDelegate, NMapLocationManagerDelegate {
+class LocationSearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
 
     }
-
-    
-    func locationManager(_ locationManager: NMapLocationManager!, didUpdateTo location: CLLocation!) {
-        <#code#>
-    }
-
-    func locationManager(_ locationManager: NMapLocationManager!, didFailWithError errorType: NMapLocationManagerErrorType) {
-        <#code#>
-    }
-
-    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForOverlayItem poiItem: NMapPOIitem!, selected: Bool) -> UIImage! {
-        <#code#>
-    }
-
-    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, anchorPointWithType poiFlagType: NMapPOIflagType) -> CGPoint {
-        <#code#>
-    }
-
-    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForCalloutOverlayItem poiItem: NMapPOIitem!, constraintSize: CGSize, selected: Bool, imageForCalloutRightAccessory: UIImage!, calloutPosition: UnsafeMutablePointer<CGPoint>!, calloutHit calloutHitRect: UnsafeMutablePointer<CGRect>!) -> UIImage! {
-        <#code#>
-    }
-
-    func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, calloutOffsetWithType poiFlagType: NMapPOIflagType) -> CGPoint {
-        <#code#>
-    }
-
-    func onMapView(_ mapView: NMapView!, initHandler error: NMapError!) {
-        <#code#>
-    }
-
-
 
 
 
