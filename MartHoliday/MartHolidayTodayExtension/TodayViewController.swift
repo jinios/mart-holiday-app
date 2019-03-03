6117a2a9a1ee80c886e361ddc82dcb859ec9da1e
Mijin Jeon <mayihelpu27@gmail.com>
2019-03-03 17:42:15 +0900

    Fix: Use keyPath for parsing BranchData
    for todayExtension app

--- MartHoliday/MartHolidayTodayExtension/TodayViewController.swift
+++ MartHoliday/MartHolidayTodayExtension/TodayViewController.swift
@@ -58,18 +58,21 @@ class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDeleg
         let idstr = ids.map{String($0)}.joined(separator: ",")
 
         guard let urlStr = appGroup?.value(forKey: KeyInfo.FavoriteBranchesURL.rawValue) as? String else { return }
-        guard let baseURL = URL(string: urlStr) else { return }
-        let url = baseURL.appendingPathComponent(idstr)
+
+        var urlComp = URLComponents(string: urlStr)
+        urlComp?.queryItems = [URLQueryItem(name: "ids", value: idstr)]
+        guard let url = urlComp?.url else {return}
 
         let configure = URLSessionConfiguration.default
-        configure.timeoutIntervalForRequest = 3
+        configure.timeoutIntervalForRequest = 5
         let session = URLSession(configuration: configure)
 
         session.dataTask(with: url) { [weak self] (data, response, error) in
             if let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode, let data = data {
                 var branches = [BranchRawData]()
                 do {
-                    branches = try JSONDecoder().decode([BranchRawData].self, from: data)
+                    branches = try JSONDecoder().decodeWithKeypath([BranchRawData].self, from: data, keyPath: "data")
+//                    branches = try JSONDecoder().decode([BranchRawData].self, from: data)
                     self?.favoriteList = BranchList(branches: branches)
                     handler(true)
                 } catch {
@@ -118,4 +121,16 @@ class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDeleg
         }
     }
 
+}
+
+extension JSONDecoder {
+    func decodeWithKeypath<T: Decodable>(_ type: T.Type, from data: Data, keyPath: String) throws -> T {
+        let toplevel = try JSONSerialization.jsonObject(with: data)
+        if let nestedJson = (toplevel as AnyObject).value(forKeyPath: keyPath) {
+            let nestedJsonData = try JSONSerialization.data(withJSONObject: nestedJson)
+            return try decode(type, from: nestedJsonData)
+        } else {
+            throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Nested json not found for key path \"\(keyPath)\""))
+        }
+    }
 }
