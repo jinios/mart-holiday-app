30a0870db921ef8bbde4aa3130d56d4aa8fe8e94
Mijin Jeon <mayihelpu27@gmail.com>
2019-03-27 01:36:41 +0900

    Hotfix: fix bug that any data isn't show up on widget
    - Indicate ketpath in JSON decode methods

--- MartHoliday/MartHoliday/Utility/Extensions.swift
+++ MartHoliday/MartHoliday/Utility/Extensions.swift
@@ -205,16 +205,3 @@ extension UIDevice {
     }
 
 }
-
-extension JSONDecoder {
-    func decode<T: Decodable>(_ type: T.Type, from data: Data, keyPath: String) throws -> T {
-        let toplevel = try JSONSerialization.jsonObject(with: data)
-        if let nestedJson = (toplevel as AnyObject).value(forKeyPath: keyPath) {
-            let nestedJsonData = try JSONSerialization.data(withJSONObject: nestedJson)
-            return try decode(type, from: nestedJsonData)
-        } else {
-            throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Nested json not found for key path \"\(keyPath)\""))
-        }
-    }
-}
-
