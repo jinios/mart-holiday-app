5a17649b6ae5e734acece8f49e6f92bbf0570ec4
Mijin Jeon <mayihelpu27@gmail.com>
2019-06-02 23:40:27 +0900

    Add images for distanceSlider on,off button

---
+++ MartHoliday/MartHoliday/View/DistanceSlider.swift
@@ -0,0 +1,21 @@
+//
+//  DistanceSlider.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 02/06/2019.
+//  Copyright © 2019 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class DistanceSlider: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
