892385f4527c58b701d095b41d265a01b7fe79d5
Mijin Jeon <mayihelpu27@gmail.com>
2019-09-04 00:54:56 +0900

    Feat: Add assets, new swift file #90

---
+++ MartHoliday/MartHoliday/ViewController/SettingFavoriteListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingFavoriteListViewController.swift
+//  MartHoliday
+//
+//  Created by YOUTH2 on 04/09/2019.
+//  Copyright © 2019 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class SettingFavoriteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
